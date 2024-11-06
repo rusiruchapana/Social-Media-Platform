a4fc6ce6b4ab175060975d92ab997991e146d2b6
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-11-06 19:33:07 +0530

    configure mysql.

--- social_media_platform/social_media_platform/settings.py
+++ social_media_platform/social_media_platform/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'social_media_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
